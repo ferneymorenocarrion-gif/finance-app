8988fd684e6790df04d8fe054cee57d5d9364cf2
ferney moreno <ferney.moreno.carrion@gmail.com>
2026-05-29 18:56:37 -0500

    create tabs to separate amounts

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     import NewAmount from './components/newAmount/NewAmount.svelte';
     import {amounts, amountsKey} from './store/amounts';
     import { formatDate } from './utils/dateFormatting';
@@ -7,14 +7,25 @@
     import reload from './assets/reload.svg';
     import amount from './assets/coin.svg';
     import arrow from './assets/arrow.svg';
+    import remove from './assets/remove.svg';
+    import edit from './assets/edit.svg';
+    import AmountForm from './components/amountForm/AmountForm.svelte';
+    import type { Amount } from './types/common';
 
     let disabled = true;
     let isPopupOpen = false;
     let isNewAmountPopupOpen = false;
+    let isAmountFormPopupOpen = false;
     let isSummaryClosed = false;
+    let activeTab = 'all';
 
     $: expenses = $amounts.filter(amount => amount.type === 'expense');
     $: incomes = $amounts.filter(amount => amount.type === 'income');
+
+    $: items = $amounts.filter(amount => {
+      if (activeTab === 'all') return amount;
+      return amount.type === activeTab
+    })
 
     $: totalExpenses = expenses.reduce((acc, {amount}) => {
       return acc += Number(amount);
@@ -45,12 +56,50 @@
     const handleToggleSummary = () => {
       isSummaryClosed = !isSummaryClosed;
     }
+
+    const handleActiveTab = (e: any) => {
+      const value = e.target.value;
+
+      switch(value) {
+        case 'all':
+          activeTab = value;
+          break;
+        case 'expense':
+          activeTab = value;
+          break;
+        case 'income':
+          activeTab = value;
+          break;
+      }
+    }
+
+    const handleRemoveItem = (id: string) => {
+      const filteredAmounts = $amounts.filter(item => item.id !== id);
+      amounts.set(filteredAmounts);
+    }
+
+    let itemToEdit: Amount = {description: '', amount: '', amountCreation: '', type: '', id: ''};
+
+    const handleEditItem = (id: string) => {
+      isAmountFormPopupOpen = true;
+      itemToEdit = $amounts.find(item => item.id === id) || {description: '', amount: '', amountCreation: '', type: '', id: ''};
+      console.log('itemToEdit', itemToEdit);
+    }
+
 </script>
 
 {#if isNewAmountPopupOpen}
 <div class="new-amount-popup">
   <div class="new-amount-popup__content">
     <NewAmount bind:isNewAmountPopupOpen={isNewAmountPopupOpen}></NewAmount>
+  </div>
+</div>
+{/if}
+
+{#if isAmountFormPopupOpen}
+<div class="new-amount-popup">
+  <div class="new-amount-popup__content">
+    <AmountForm bind:isAmountFormPopupOpen={isAmountFormPopupOpen} itemToEdit={itemToEdit}></AmountForm>
   </div>
 </div>
 {/if}
@@ -80,59 +129,35 @@
   <button class="reload nav__item" on:click={handlePopup}>
     <img src={reload} alt="reload app"/>
     <span>reset</span></button>
-  <button class="add-amount nav__item" on:click={handleOpenNewAmount}><img src={amount} alt="my bussiness"/><span>new amount</span></button>
+  <button class="add-amount nav__item" on:click={handleOpenNewAmount}><img src={amount} alt="my bussiness"/><span>amount</span></button>
   <button disabled class:button-disabled={disabled} class="my-bussiness nav__item" on:click={handleClearStorage}><img src={bussiness} alt="my bussiness"/><span>bussiness</span></button>
 </nav>
-<h3>Expenses</h3>
-<table class="expenses">
-  <thead>
-    <tr>
-      <th>description</th>
-      <th>value</th>
-      <th>date</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#if expenses.length > 0}
-      {#each expenses as expense}
-      <tr>
-        <td>{expense.description}</td>
-        <td>{formatAmount(expense.amount)}</td>
-        <td>{formatDate(expense.amountCreation)}</td>
-      </tr>
+<div class="summary-history">
+  <div class="summary-history__tabs">
+    <button value="all" class:isActive={activeTab === 'all'} on:click={handleActiveTab}>All</button>
+    <button value="expense" class:isActive={activeTab === 'expense'} on:click={handleActiveTab}>Expenses</button>
+    <button value="income" class:isActive={activeTab === 'income'} on:click={handleActiveTab}>Incomes</button>
+  </div>
+  <ul class="summary-history__items">
+   {#if items.length > 0}
+      {#each items as item}
+        <li class={`summary-history__item summary-history__item--${item.type === 'expense' ? 'expense': 'income'}`}>
+          <span class={`summary-history__item-title summary-history__item-title--${item.type === 'expense' ? 'expense': 'income'}`}>{item.description}</span>
+          <div class="summary-history__item-data">
+            <span class="summary-history__item-data-amount">{formatAmount(item.amount)}</span>
+            <span class="summary-history__item-data-date">{formatDate(item.amountCreation)}</span>
+          </div>
+          <div class="summary-history__item-icons">
+            <button class="summary-history__item-icons-icon summary-history__item-icons-edit" on:click={() => handleEditItem(item.id)}><img src={edit} alt="edit field"/></button>
+            <button class="summary-history__item-icons-icon summary-history__item-icons-remove" on:click={() => handleRemoveItem(item.id)}><img src={remove} alt="remove field"/></button>
+          </div>
+        </li>
       {/each}
-    {:else}
-      <tr>
-      <td colspan="3">no data added yet</td>
-    </tr>
+    {:else} 
+    <p style="margin: 15px 0px;">no data added yet</p>
     {/if}
-  </tbody>
-</table>
-<h3>Incomes</h3>
-<table class="incomes">
-  <thead>
-    <tr>
-      <th>description</th>
-      <th>value</th>
-      <th>date</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#if incomes.length > 0}
-      {#each incomes as income}
-      <tr>
-        <td>{income.description}</td>
-        <td>{formatAmount(income.amount)}</td>
-        <td>{formatDate(income.amountCreation)}</td>
-      </tr>
-      {/each}
-    {:else}
-      <tr>
-      <td colspan="3">no data added yet</td>
-    </tr>
-    {/if}
-  </tbody>
-</table>
+  </ul>
+</div>
 <!-- <div class="total-expenses">
   <span>total expenses</span>
   <span>{formatAmount(totalExpenses)}</span>
@@ -141,27 +166,139 @@
 
 <style lang="scss">
 
+  .summary-history {
+    width: 100%;
+    background-color: #e8e8e8;
+    max-height: 320px;
+    border-radius: 15px;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+
+    &__tabs {
+      margin: 15px 0;
+
+      button {
+        min-width: 90px;
+        padding: 10px 15px;
+        border-radius: 20px;
+        background: #ffffff;
+        border: 1px solid #9fa1a2;
+        font-size: 15px;
+        color: #666666;
+      }
+
+      button.isActive {
+        background-color: #8c8c8c;
+        color: #ffffff;
+      }
+    }
+    
+    &__items{
+      display: flex;
+      flex-direction: column;
+      margin: 0 auto;
+      padding: 0;
+      align-items: center;
+      width: 100%;
+      overflow-y: scroll;
+    }
+
+    &__item {
+      list-style: none;
+      background-color: #ffffff;
+      width: 90%;
+      padding: 0 10px;
+      border-radius: 15px;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      margin-bottom: 15px;
+      position: relative;
+
+      &:after {
+        content: '';
+        position: absolute;
+        width: 15px;
+        height: 15px;
+        right: 20px;
+        border-radius: 20px;
+        top: 9px;
+      }
+
+      &--income {
+        &:after {
+          background-color: #35b368;
+        }
+      }
+
+      &--expense {
+        &:after {
+          background-color: #fb7979;
+        }
+      }
+    }
+
+    &__item-title {
+      font-size: 16px;
+      text-align: left;
+      width: 100%;
+      display: block;
+      padding: 5px 10px;
+      border-bottom: 1px solid #cccccc;
+      text-transform: capitalize;
+      font-weight: 800;
+    }
+
+    &__item-data {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      margin: 10px 0;
+      padding-left: 10px;
+      line-height: 20px;
+    }
+
+    &__item-data-amount {
+      font-size: 18px;
+    }
+
+    &__item-data-date {
+      font-size: 12px;
+    }
+
+    &__item-icons {
+      display: flex;
+      align-items: flex-start;
+      margin: 10px 0;
+      padding-right: 10px;
+      line-height: 20px;
+    }
+
+    &__item-icons-icon {
+      width: 35px;
+      height: 35px;
+      background: none;
+      border: 1px solid #cccccc;
+      border-radius: 20px;
+
+      &:first-child {
+        margin-right: 10px;
+      }
+
+      img {
+        width: 100%;
+      }
+    }
+  }
+
   .finance-app {
-    padding: 5%;
+    padding: 5% 2%;
   }
   h3 {
     color: #ffffff;
     text-align: left;
     margin: 15px 0 5px;
-  }
-  table, td, th {
-    border: 1px solid;
-    padding: 10px;
-  }
-
-  th {
-    text-transform: capitalize;
-  }
-
-  table {
-    border-collapse: collapse;
-    width: 100%;
-    color: #3c4143;
   }
 
   .expenses {
